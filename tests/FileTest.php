77859ccf294dca9e2213ddb76492cbbbd78a6839
domenico <domenico@translated.net>
2024-05-30 17:35:41 +0200

    Excelude test if loding fail.

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -13,6 +13,34 @@ class FileTest extends BaseTest
         $url = 'https://jsonplaceholder.typicode.com/photos';
         $content = json_decode(File::loadFile($url));
 
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                $msg = null;
+                break;
+            case JSON_ERROR_DEPTH:
+                $msg = ' - Maximum stack depth exceeded';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $msg = ' - Underflow or the modes mismatch';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $msg = ' - Unexpected control character found';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $msg = ' - Syntax error, malformed JSON';
+                break;
+            case JSON_ERROR_UTF8:
+                $msg = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                break;
+            default:
+                $msg = ' - Unknown error';
+                break;
+        }
+
+        if($msg != null){
+            $this->markTestSkipped($msg);
+        }
+
         $this->assertTrue(is_array($content));
         $this->assertCount(5000, $content);
     }
